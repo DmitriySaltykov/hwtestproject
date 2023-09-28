5923cd8d1237ff64e6e589c00e2558846d8d5986
DSaltykov <spitfiredia@gmail.com>
2023-09-28 14:29:30 +0300

    reformat selenidelement

--- src/test/java/SwagLabsTest/pages/CheckPage.java
+++ src/test/java/SwagLabsTest/pages/CheckPage.java
@@ -1,30 +1,36 @@
 package SwagLabsTest.pages;
 
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CheckPage {
+    SelenideElement shopingicon = $("#shopping_cart_container"),
+            cartitem = $(".cart_item"),
+            checkoutbtn = $("#checkout"),
+            craatorder = $(By.id("continue")),
+            checkoutnotification = $(By.id("continue"));
 
 
     //Нажатие на кнопку корзины
     public CheckPage OpenCheckout() {
-        $("#shopping_cart_container").click();
+        shopingicon.click();
 
         return this;
     }
 
     //Проверка наличия товара в корзине
     public CheckPage CheckItem() {
-        $(".cart_item").should(exist);
+        cartitem.should(exist);
 
         return this;
     }
 
     //Клик на кнопку чекаута
     public CheckPage CheckoutClick() {
-        $("#checkout").click();
+        checkoutbtn.click();
 
 
         return this;
@@ -32,14 +38,14 @@ public class CheckPage {
 
     // Клик на кнопку продолжить
     public CheckPage ContinueClick() {
-        $(By.id("continue")).click();
+        craatorder.click();
         // $("[id='continue']").click();
 
         return this;
     }
 
     public CheckPage checkoutNotification(String value) {
-        $("#checkout_info_container > div > form > div.checkout_info > div.error-message-container.error > h3").shouldHave(text(value));
+        checkoutnotification.shouldHave(text(value));
         return this;
 
     }
