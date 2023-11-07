65126100b6b12a0d32885fb155f0c4c2ffa54ac3
DSaltykov <spitfiredia@gmail.com>
2023-11-07 17:26:23 +0300

    added step in test

--- src/test/java/SwagLabsTest/test/CheckoutTest.java
+++ src/test/java/SwagLabsTest/test/CheckoutTest.java
@@ -53,11 +53,21 @@ public class CheckoutTest extends TestBase {
     @DisplayName("Переход на страницу оплаты")
     void SwagLabsGoRoPayest() {
 
-        authPage.AccessAuthWithConfig();
-        cartPage.addToCart();
+        // Открытие страницы,успешная авторизация
+        step("Открытие страницы и успешная авторизация", () -> {
+            authPage
+                    .AccessAuthWithConfig();
+        });
+
+        step("Добавление товара в корзину ", () -> {
+            cartPage.addToCart();
+        });
+
+        step("Открытие страницы чекаута и переход на страницу оплаты ", () -> {
         checkPage.OpenCheckout()
                 .CheckItem()
                 .CheckoutClick();
+        });
 
 
     }
@@ -71,13 +81,21 @@ public class CheckoutTest extends TestBase {
     void SwagLabsCreatOrderCheckNotificationTest() {
 
         // Открытие страницы,успешная авторизация
-        authPage.AccessAuthWithConfig();
-        cartPage.addToCart();
+        step("Открытие страницы и успешная авторизация", () -> {
+            authPage
+                    .AccessAuthWithConfig();
+        });
+
+        step("Добавление товара в корзину ", () -> {
+            cartPage.addToCart();
+        });
+        step("Проверка нотификации на странице чекаута при не заполненых полях ", () -> {
         checkPage.OpenCheckout()
                 .CheckItem()
                 .CheckoutClick()
                 .ContinueClick()
                 .checkoutNotification("Error: First Name is required");
+        });
 
 
     }
@@ -90,15 +108,29 @@ public class CheckoutTest extends TestBase {
     void SucsessCreatOrderTest() {
 
         // Открытие страницы,успешная авторизация
-        authPage.AccessAuthWithConfig();
-        cartPage.addToCart();
+        step("Открытие страницы и успешная авторизация", () -> {
+            authPage
+                    .AccessAuthWithConfig();
+        });
+
+        step("Добавление товара в корзину ", () -> {
+            cartPage.addToCart();
+        });
+        step("Переход на страницу оплаты  ", () -> {
         checkPage.OpenCheckout()
-                .CheckItem()
+                .CheckItem();
+        });
+        step("Заполнение обязательных полей и оформление заказа ", () -> {
+            checkPage
                 .CheckoutClick()
                 .AccessClientInfo()
                 .ContinueClick()
-                .FinishClick()
+                .FinishClick();
+        });
+        step("Проверка наличия нотификации об успешном оформлении заказа", () -> {
+            checkPage
                 .FinalSucsessNotification("Thank you for your order!");
+        });
 
 
 
